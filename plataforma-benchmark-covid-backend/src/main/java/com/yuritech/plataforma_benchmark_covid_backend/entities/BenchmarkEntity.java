55c08bf50836c18fdac2b2ba4b4502dc1672d2f3
Yuri Ramos <yurigabriel21@yahoo.com>
2025-02-20 20:56:12 -0300

    refactor: adiciona pattern de formato de data e muda tipagem de data da entidade Benchmark

--- plataforma-benchmark-covid-backend/src/main/java/com/yuritech/plataforma_benchmark_covid_backend/entities/BenchmarkEntity.java
+++ plataforma-benchmark-covid-backend/src/main/java/com/yuritech/plataforma_benchmark_covid_backend/entities/BenchmarkEntity.java
@@ -4,10 +4,13 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import org.hibernate.annotations.CreationTimestamp;
+import org.springframework.format.annotation.DateTimeFormat;
 
+import java.sql.Timestamp;
+import java.time.Instant;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
@@ -28,10 +31,12 @@ public class BenchmarkEntity {
     @Column(name="pais_dois", length = 100)
     private String pais2;
 
-    private LocalDateTime dataInicio;
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private LocalDate dataInicio;
 
-    private LocalDateTime dataFim;
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private LocalDate dataFim;
 
     @CreationTimestamp
-    private LocalDateTime createdAt;
+    private Instant createdAt;
 }
