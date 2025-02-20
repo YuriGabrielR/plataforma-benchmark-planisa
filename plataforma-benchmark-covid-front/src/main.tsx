c7a50d81702bca4d52c5f0a4c32d55bf9b86ba76
Yuri Ramos <yurigabriel21@yahoo.com>
2025-02-20 10:22:32 -0300

    feat: implementa componentes globais de sidebar e menu.

--- plataforma-benchmark-covid-front/src/main.tsx
+++ plataforma-benchmark-covid-front/src/main.tsx
@@ -5,12 +5,15 @@ import { configRouter } from './rotas/Router'
 import { RouterProvider } from 'react-router-dom'
 import { QueryClientProvider } from '@tanstack/react-query'
 import { queryClient } from './api/apiConfig'
+import { ChakraProvider, defaultSystem } from '@chakra-ui/react'
 
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-   <QueryClientProvider client={queryClient}>  
+    <ChakraProvider value={defaultSystem}>
+    <QueryClientProvider client={queryClient}>  
     <RouterProvider router={configRouter} />
     </QueryClientProvider> 
+    </ChakraProvider>
   </StrictMode>
 )
