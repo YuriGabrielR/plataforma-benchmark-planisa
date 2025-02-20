cdda0a436f4b64eb13703fb78477e7f58a936d89
Yuri Ramos <yurigabriel21@yahoo.com>
2025-02-19 21:11:45 -0300

    config: pré configuração + adição de arquivos relacionados ao consumo de api's a partir do back-end

--- plataforma-benchmark-covid-front/src/main.tsx
+++ plataforma-benchmark-covid-front/src/main.tsx
@@ -3,10 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { configRouter } from './rotas/Router'
 import { RouterProvider } from 'react-router-dom'
+import { QueryClientProvider } from '@tanstack/react-query'
+import { queryClient } from './api/apiConfig'
 
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-  <RouterProvider router={configRouter} />
+   <QueryClientProvider client={queryClient}>  
+    <RouterProvider router={configRouter} />
+    </QueryClientProvider> 
   </StrictMode>
 )
